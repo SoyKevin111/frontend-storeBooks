bb327107752f5c8f46c37723ba0931bed7046a14
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-06-27 09:29:32 -0500

    ng imagen optimized.

--- src/app/layout/customer/components/book-container/book-container.component.ts
+++ src/app/layout/customer/components/book-container/book-container.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-book-container',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './book-container.component.html',
   styleUrl: './book-container.component.scss'
 })
