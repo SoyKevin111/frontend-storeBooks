9195df34aa4581852c9b23f8c7a74e719ffcbb76
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-06-28 00:07:39 -0500

    cambiando el estilo del navbar.

--- src/app/layout/main/components/sidebar/sidebar.component.ts
+++ src/app/layout/main/components/sidebar/sidebar.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { User } from '../../../../features/models/user.model';
 import { EventEmitterService } from '../../../../features/services/event-emitter.service';
-import {  RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
@@ -15,12 +16,29 @@ export class SidebarComponent implements OnInit {
   user!: User;
   eventEmitterService = inject(EventEmitterService);
 
+  isCollapsed = true;
+  private collapseTimeout: any;
+
+
+
   ngOnInit(): void {
     this.user = this.eventEmitterService.getUser();
     this.eventEmitterService.userChanged.subscribe(user => {
       this.user = user;
     });
   }
+
+  onSidebarEnter() {
+    clearTimeout(this.collapseTimeout);
+    this.isCollapsed = false;
+  }
+
+  onSidebarLeave() {
+    this.collapseTimeout = setTimeout(() => {
+      this.isCollapsed = true;
+    }, 200); 
+  }
+
 
 
 }
