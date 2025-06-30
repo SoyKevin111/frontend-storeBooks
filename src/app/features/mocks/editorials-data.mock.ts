23754f830128c2a6f16befb57ea937afcc1eab39
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-06-30 12:48:58 -0500

    arreglando validaciones v2

--- src/app/features/mocks/editorials-data.mock.ts
+++ src/app/features/mocks/editorials-data.mock.ts
@@ -1,15 +1,14 @@
 import { Editorial } from "../models/editorial.model";
 
 export const MOCK_EDITORIALS: Editorial[] = [
-  { id: 1, name: 'Addison-Wesley', phone: '123-456-7890', website: 'https://www.awl.com', email: 'contact@awl.com', state: 'Massachusetts' },
-  { id: 2, name: 'O’Reilly Media', phone: '987-654-3210', website: 'https://www.oreilly.com', email: 'info@oreilly.com', state: 'California' },
-  { id: 3, name: 'HarperOne', phone: '800-123-4567', website: 'https://www.harperone.com', email: 'support@harperone.com', state: 'New York' },
-  { id: 4, name: 'Harper', phone: '888-456-7890', website: 'https://www.harper.com', email: 'contact@harper.com', state: 'New Jersey' },
-  { id: 5, name: 'Celadon Books', phone: '321-654-9870', website: 'https://www.celadonbooks.com', email: 'info@celadon.com', state: 'New York' },
-  { id: 6, name: 'G.P. Putnam’s Sons', phone: '999-888-7777', website: 'https://www.penguinrandomhouse.com', email: 'help@putnam.com', state: 'New York' },
-  { id: 7, name: 'Prentice Hall', phone: '800-555-1212', website: 'https://www.pearson.com', email: 'info@prentice.com', state: 'New Jersey' },
-  { id: 8, name: 'Addison-Wesley Professional', phone: '123-123-1234', website: 'https://www.awl.com/professional', email: 'editorial@awl.com', state: 'Massachusetts' },
-  { id: 9, name: 'Penguin Random House', phone: '877-777-7777', website: 'https://www.penguinrandomhouse.com', email: 'prh@penguin.com', state: 'New York' },
-  { id: 10, name: 'Crown Publishing', phone: '800-999-0000', website: 'https://www.crownpublishing.com', email: 'info@crown.com', state: 'New York' }
-
+  { id: 1, name: 'Addison-Wesley', phone: '1234567890', website: 'https://www.awl.com', email: 'contact@awl.com', state: 'Massachusetts' },
+  { id: 2, name: 'O’Reilly Media', phone: '9876543210', website: 'https://www.oreilly.com', email: 'info@oreilly.com', state: 'California' },
+  { id: 3, name: 'HarperOne', phone: '8001234567', website: 'https://www.harperone.com', email: 'support@harperone.com', state: 'New York' },
+  { id: 4, name: 'Harper', phone: '8884567890', website: 'https://www.harper.com', email: 'contact@harper.com', state: 'New Jersey' },
+  { id: 5, name: 'Celadon Books', phone: '3216549870', website: 'https://www.celadonbooks.com', email: 'info@celadon.com', state: 'New York' },
+  { id: 6, name: 'G.P. Putnam’s Sons', phone: '9998887777', website: 'https://www.penguinrandomhouse.com', email: 'help@putnam.com', state: 'New York' },
+  { id: 7, name: 'Prentice Hall', phone: '8005551212', website: 'https://www.pearson.com', email: 'info@prentice.com', state: 'New Jersey' },
+  { id: 8, name: 'Addison-Wesley Professional', phone: '1231231234', website: 'https://www.awl.com/professional', email: 'editorial@awl.com', state: 'Massachusetts' },
+  { id: 9, name: 'Penguin Random House', phone: '8777777777', website: 'https://www.penguinrandomhouse.com', email: 'prh@penguin.com', state: 'New York' },
+  { id: 10, name: 'Crown Publishing', phone: '8009990000', website: 'https://www.crownpublishing.com', email: 'info@crown.com', state: 'New York' }
 ];
