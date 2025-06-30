a84dc84bf6dba9c61d784423b674777fba937ddb
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-06-30 15:54:40 -0500

    puntos eguro

--- src/app/layout/admin/page/invoice/invoice.component.ts
+++ src/app/layout/admin/page/invoice/invoice.component.ts
@@ -3,12 +3,13 @@ import { Router } from '@angular/router';
 import { CrudTableComponent } from '../../../../shared/components/crud-table/crud-table.component';
 import { Invoice } from '../../../../features/models/invoice.model';
 import { MOCK_INVOICES } from '../../../../features/mocks/invoice-data.mocks';
+import { FormInvoiceComponent } from '../../components/form-invoice/form-invoice.component';
 
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [CrudTableComponent],
+  imports: [CrudTableComponent, FormInvoiceComponent],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.scss'
 })
