3e2b93592d1605c5c605d3a2c171bfe0b1606a67
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-07-15 21:48:59 -0500

    nada

--- generate-db.js
+++ generate-db.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const dataDir = path.join(__dirname, 'database');
 
-const dataFiles = ['customers.json', 'administrators.json', 'editorials.json', 'authors.json', 'books.json', 'invoices.json'];
+const dataFiles = ['administrators.json'];
 const db = {};
 
 dataFiles.forEach((file) => {
