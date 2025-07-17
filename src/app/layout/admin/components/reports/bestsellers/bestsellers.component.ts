045df87828fd27785163e6cb23631653ac31da28
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-07-17 18:11:57 -0500

    casi finalizado el details 4

--- src/app/layout/admin/components/reports/bestsellers/bestsellers.component.ts
+++ src/app/layout/admin/components/reports/bestsellers/bestsellers.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { mockBestSellers } from '../../../../../features/mocks/reports-data.mock';
+import { RandomColorEmojiService } from '../../../../../shared/services/random-color-emoji.service';
 
 @Component({
   selector: 'app-bestsellers',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bestsellers.component.html',
   styleUrl: './bestsellers.component.scss'
 })
 export class BestsellersComponent {
+
+  color = inject(RandomColorEmojiService);
+
+  data$ = mockBestSellers;
 
 }
--- src/app/layout/admin/page/reports/reports.component.ts
+++ src/app/layout/admin/page/reports/reports.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MonthySalesComponent } from '../../components/reports/monthy-sales/monthy-sales.component';
 import { LowRotationBooksComponent } from '../../components/reports/low-rotation-books/low-rotation-books.component';
 import { BestsellersByCategoryComponent } from '../../components/reports/bestsellers-by-category/bestsellers-by-category.component';
+import { BestsellersComponent } from '../../components/reports/bestsellers/bestsellers.component';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [MonthySalesComponent, LowRotationBooksComponent, BestsellersByCategoryComponent],
+  imports: [MonthySalesComponent, LowRotationBooksComponent, BestsellersByCategoryComponent, BestsellersComponent],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
