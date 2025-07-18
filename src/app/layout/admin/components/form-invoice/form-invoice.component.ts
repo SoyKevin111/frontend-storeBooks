39b6c0e04ffe8cbd58235aba732d708918b6186b
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-07-18 08:16:54 -0500

    ajustando algunos disenos

--- src/app/layout/admin/components/form-invoice/form-invoice.component.ts
+++ src/app/layout/admin/components/form-invoice/form-invoice.component.ts
@@ -109,7 +109,7 @@ export class FormInvoiceComponent implements OnInit, OnDestroy {
   onSearchBook() {
     const term = this.searchBookText.toLowerCase();
     this.filteredBooks = this.books$.filter(b =>
-      b.title.toLowerCase().includes(term) || b.isbn.includes(term)
+      b.title.toLowerCase().includes(term) || b.isbn.includes(term) || b.category.toLowerCase().includes(term)
     );
   }
 
